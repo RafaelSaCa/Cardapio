ddd366d6912f6adeaef566f0c46293a2c066f2c0
Rafael Saca <raffaellsaca@gmail.com>
2024-11-08 22:47:28 -0300

    feat: Register Controller

--- src/main/java/com/rafaelsaca/cardapio/controllers/RegisterController.java
+++ src/main/java/com/rafaelsaca/cardapio/controllers/RegisterController.java
@@ -3,7 +3,6 @@ package com.rafaelsaca.cardapio.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
